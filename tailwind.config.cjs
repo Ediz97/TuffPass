3b9ca5c0e74d7d55ba9c0b123d986675ea11859b
Ediz Turan <edizturan97@gmail.com>
2023-02-26 01:07:16 +0100

    custom breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte}'],
   theme: {
+    screens: {
+      'xs': '400px',
+      'sm': '800px',
+      'md': '1200px',
+      'lg': '1600px',
+      'xl': '2000px',
+      '2xl': '2400px',
+      '3xl': '2800px',
+      '4xl': '3200px',
+      '5xl': '3600px',
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
