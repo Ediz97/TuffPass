dfe81e9d3757435486c73040f74e275018e73284
Ediz Turan <edizturan97@gmail.com>
2023-03-05 17:20:17 +0100

    added clear button

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -1,14 +1,17 @@
 <script>
     import { userAccounts } from "./stores";
 
-    let searchPrompt = '';
+    let searchPrompt = "";
 
     function filterCards() {
         for (let i = 0; i < $userAccounts.length; i++) {
-            if (!$userAccounts[i].cardName.toLowerCase().includes(searchPrompt.toLowerCase())) {
+            if (
+                !$userAccounts[i].cardName
+                    .toLowerCase()
+                    .includes(searchPrompt.toLowerCase())
+            ) {
                 $userAccounts[i].visible = false;
-            }
-            else {
+            } else {
                 $userAccounts[i].visible = true;
             }
         }
@@ -16,7 +19,34 @@
 </script>
 
 <div class="flex justify-center mb-5 px-2">
-    <input type="text" placeholder="Type to search" class="input input-ghost border-primary border-2 w-full max-w-lg"
-    bind:value={searchPrompt}
-    on:input={filterCards} />
+    <div class="input-group justify-center">
+        <input
+            type="text"
+            placeholder="Search…"
+            class="input input-ghost input-bordered border-2 w-full max-w-lg"
+            bind:value={searchPrompt}
+            on:input={filterCards}
+        />
+        <button
+            class="btn btn-error bg-transparent border-2 border-opacity-30"
+            on:click={() => {
+                searchPrompt = "";
+                filterCards();
+            }}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 opacity-70"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                /></svg
+            >
+        </button>
+    </div>
 </div>
