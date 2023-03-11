25396176303e08bf98d5592a136130b3a8f8a3b0
Ediz Turan <edizturan97@gmail.com>
2023-03-11 21:27:24 +0100

    replaced font

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 	import NavigationBar from "./lib/NavigationBar.svelte";
 </script>
 
-<main class="my-5">
+<main class="my-5" style="font-family: 'Caviar Dreams', sans-serif;">
 	<NavigationBar />
 	<AccountCards />
 </main>
