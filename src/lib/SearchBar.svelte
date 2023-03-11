b85923bd15cd4d459dcb111b5c3f017dddd02439
Ediz Turan <edizturan97@gmail.com>
2023-03-11 17:01:15 +0100

    added navigation bar

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -18,35 +18,45 @@
     }
 </script>
 
-<div class="flex justify-center mb-5 px-2">
-    <div class="input-group justify-center">
-        <input
-            type="text"
-            placeholder="Search…"
-            class="input input-ghost input-bordered border-2 w-full max-w-lg"
-            bind:value={searchPrompt}
-            on:input={filterCards}
-        />
-        <button
-            class="btn btn-error bg-transparent border-2 border-opacity-30"
-            on:click={() => {
-                searchPrompt = "";
-                filterCards();
-            }}
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 opacity-70"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                /></svg
-            >
-        </button>
+<div class="mx-5 mb-5 text-neutral-content">
+    <div
+        class="navbar bg-primary shadow-xl rounded-xl px-5"
+    >
+        <h1 class="normal-case text-xl select-none pr-5 font-bold">TuffPass</h1>
+        <div class="ml-auto max-w-sm w-full">
+            <div class="input-group pr-5">
+                <input
+                    type="text"
+                    placeholder="Search…"
+                    class="input input-ghost input-bordered border-2 w-full"
+                    bind:value={searchPrompt}
+                    on:input={filterCards}
+                />
+                <button
+                    class="btn btn-error bg-transparent border-2 border-opacity-30"
+                    on:click={() => {
+                        searchPrompt = "";
+                        filterCards();
+                    }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6 opacity-70"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M6 18L18 6M6 6l12 12"
+                        /></svg
+                    >
+                </button>
+            </div>
+            <button class="btn btn-ghost btn-circle btn-sm">
+                <img src="https://img.icons8.com/external-justicon-lineal-color-justicon/64/null/external-color-palette-graphic-design-justicon-lineal-color-justicon.png" alt="Themes"/>
+            </button>
+        </div>
     </div>
 </div>
