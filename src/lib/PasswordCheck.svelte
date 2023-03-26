cfbb4c7b18e97074dcad12c62323f7ba7522ee0f
Ediz Turan <edizturan97@gmail.com>
2023-03-26 15:05:39 +0200

    added more symbols

--- src/lib/PasswordCheck.svelte
+++ src/lib/PasswordCheck.svelte
@@ -1,114 +1,111 @@
 <script context="module">
-    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const symbols = '!@#$%^&*';
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  const symbols = `!@#$%^&*.,-_'/()[]{}?<>:;=+`;
 
-    export function passwordCheck(password) {
-        if (password.length === 0) {
-            return [0, '<br>'];
-        }
-
-        if (password.length <= 7) {
-            return [25, 'Weak'];
-        }
-
-        if (password.length <= 10) {
-            if (amountOfCharacterTypes(password) <= 2) {
-                return [25, 'Weak'];
-            } else if (amountOfCharacterTypes(password) === 3) {
-                return [50, 'Medium'];
-            } else {
-                return [75, 'Strong'];
-            }
-        }
-
-        if (password.length <= 13) {
-            if (amountOfCharacterTypes(password) === 1) {
-                return [25, 'Weak'];
-            } else if (amountOfCharacterTypes(password) === 2) {
-                return [50, 'Medium'];
-            } else if (amountOfCharacterTypes(password) === 3) {
-                return [75, 'Strong'];
-            } else {
-                return [100, 'Very Strong'];
-            }
-        }
-
-        if (password.length >= 14) {
-            if (amountOfCharacterTypes(password) === 1) {
-                return [25, 'Weak'];
-            } else if (amountOfCharacterTypes(password) === 2) {
-                return [50, 'Medium'];
-            } else {
-                return [100, 'Very Strong'];
-            }
-        }
+  export function passwordCheck(password) {
+    if (password.length === 0) {
+      return [0, '<br>'];
     }
 
-    function amountOfCharacterTypes(password) {
-        let amountOfCharacterTypes = 0;
-        for (let i = 0; i < password.length; i++) {
-            if (lowercaseLetters.includes(password[i])) {
-                amountOfCharacterTypes++;
-                break;
-            }
-        }
-        for (let i = 0; i < password.length; i++) {
-            if (uppercaseLetters.includes(password[i])) {
-                amountOfCharacterTypes++;
-                break;
-            }
-        }
-        for (let i = 0; i < password.length; i++) {
-            if (numbers.includes(password[i])) {
-                amountOfCharacterTypes++;
-                break;
-            }
-        }
-        for (let i = 0; i < password.length; i++) {
-            if (symbols.includes(password[i])) {
-                amountOfCharacterTypes++;
-                break;
-            }
-        }
-        return amountOfCharacterTypes;
+    if (password.length <= 7) {
+      return [25, 'Weak'];
     }
 
-    export function generatePassword() {
-        let generatedPassword = '';
-        do {
-            generatedPassword = '';
-            for (let i = 0; i < 14; i++) {
-                switch (Math.ceil(Math.random() * 4)) {
-                    case 1:
-                        generatedPassword +=
-                            lowercaseLetters[
-                                Math.floor(
-                                    Math.random() * lowercaseLetters.length
-                                )
-                            ];
-                        break;
-                    case 2:
-                        generatedPassword +=
-                            uppercaseLetters[
-                                Math.floor(
-                                    Math.random() * uppercaseLetters.length
-                                )
-                            ];
-                        break;
-                    case 3:
-                        generatedPassword +=
-                            numbers[Math.floor(Math.random() * numbers.length)];
-                        break;
-                    case 4:
-                        generatedPassword +=
-                            symbols[Math.floor(Math.random() * symbols.length)];
-                        break;
-                }
-            }
-        } while (amountOfCharacterTypes(generatedPassword) !== 4);
-
-        return generatedPassword;
+    if (password.length <= 10) {
+      if (amountOfCharacterTypes(password) <= 2) {
+        return [25, 'Weak'];
+      } else if (amountOfCharacterTypes(password) === 3) {
+        return [50, 'Medium'];
+      } else {
+        return [75, 'Strong'];
+      }
     }
+
+    if (password.length <= 13) {
+      if (amountOfCharacterTypes(password) === 1) {
+        return [25, 'Weak'];
+      } else if (amountOfCharacterTypes(password) === 2) {
+        return [50, 'Medium'];
+      } else if (amountOfCharacterTypes(password) === 3) {
+        return [75, 'Strong'];
+      } else {
+        return [100, 'Very Strong'];
+      }
+    }
+
+    if (password.length >= 14) {
+      if (amountOfCharacterTypes(password) === 1) {
+        return [25, 'Weak'];
+      } else if (amountOfCharacterTypes(password) === 2) {
+        return [50, 'Medium'];
+      } else {
+        return [100, 'Very Strong'];
+      }
+    }
+  }
+
+  function amountOfCharacterTypes(password) {
+    let amountOfCharacterTypes = 0;
+
+    for (let char of password) {
+        if (lowercaseLetters.includes(char)) {
+        amountOfCharacterTypes++;
+        break;
+        }
+    }
+    for (let char of password) {
+        if (uppercaseLetters.includes(char)) {
+        amountOfCharacterTypes++;
+        break;
+        }
+    }
+    for (let char of password) {
+        if (numbers.includes(char)) {
+        amountOfCharacterTypes++;
+        break;
+        }
+    }
+    for (let char of password) {
+        if (symbols.includes(char)) {
+        amountOfCharacterTypes++;
+        break;
+        }
+    }
+    return amountOfCharacterTypes;
+  }
+
+  export function generatePassword() {
+    let generatedPassword = '';
+    do {
+      generatedPassword = '';
+      for (let i = 0; i < 14; i++) {
+        switch (Math.ceil(Math.random() * 4)) {
+          case 1:
+            generatedPassword +=
+              lowercaseLetters[
+                Math.floor(Math.random() * lowercaseLetters.length)
+              ];
+            break;
+          case 2:
+            generatedPassword +=
+              uppercaseLetters[
+                Math.floor(Math.random() * uppercaseLetters.length)
+              ];
+            break;
+          case 3:
+            generatedPassword +=
+              numbers[Math.floor(Math.random() * numbers.length)];
+            break;
+          case 4:
+            generatedPassword +=
+              symbols[Math.floor(Math.random() * symbols.length)];
+            break;
+        }
+      }
+    } while (amountOfCharacterTypes(generatedPassword) !== 4);
+
+    return generatedPassword;
+  }
 </script>
