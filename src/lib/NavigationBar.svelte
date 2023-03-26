c82cc227744d1ecc4f565c851364b02bffc673b4
Ediz Turan <edizturan97@gmail.com>
2023-03-26 15:07:28 +0200

    fixed indents

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,61 +1,63 @@
 <script>
-    import { userAccounts } from "./stores";
-    import ThemePicker from "./ThemePicker.svelte";
+  import { userAccounts } from "./stores";
+  import ThemePicker from "./ThemePicker.svelte";
 
-    let searchPrompt = "";
+  let searchPrompt = "";
 
-    function filterCards() {
-        for (let i = 0; i < $userAccounts.length; i++) {
-            if (
-                !$userAccounts[i].cardName
-                    .toLowerCase()
-                    .includes(searchPrompt.toLowerCase())
-            ) {
-                $userAccounts[i].visible = false;
-            } else {
-                $userAccounts[i].visible = true;
-            }
-        }
+  function filterCards() {
+    for (let i = 0; i < $userAccounts.length; i++) {
+      if (
+        !$userAccounts[i].cardName
+          .toLowerCase()
+          .includes(searchPrompt.toLowerCase())
+      ) {
+        $userAccounts[i].visible = false;
+      } else {
+        $userAccounts[i].visible = true;
+      }
     }
+  }
 </script>
 
-<div class="mx-3 mb-5">
-    <div
-        class="navbar bg-primary shadow-xl px-5"
+<div class="mx-3 my-5">
+  <div class="navbar bg-primary shadow-xl px-5">
+    <h1
+      class="normal-case text-xl select-none pr-5 font-black text-primary-content tracking-wide"
     >
-        <h1 class="normal-case text-xl select-none pr-5 font-black text-primary-content tracking-wide">TuffPass</h1>
-        <div class="ml-auto max-w-sm w-full">
-            <div class="input-group pr-5">
-                <input
-                    type="text"
-                    placeholder="Search…"
-                    class="input input-ghost input-bordered border-2 w-full"
-                    bind:value={searchPrompt}
-                    on:input={filterCards}
-                />
-                <button
-                    class="btn btn-error bg-transparent border-2 border-opacity-30"
-                    on:click={() => {
-                        searchPrompt = "";
-                        filterCards();
-                    }}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 opacity-70"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        ><path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12"
-                        /></svg
-                    >
-                </button>
-            </div>
-            <ThemePicker />
-        </div>
+      TuffPass
+    </h1>
+    <div class="ml-auto max-w-sm w-full">
+      <div class="input-group pr-5">
+        <input
+          type="text"
+          placeholder="Search…"
+          class="input input-ghost input-bordered border-2 w-full"
+          bind:value={searchPrompt}
+          on:input={filterCards}
+        />
+        <button
+          class="btn btn-error bg-transparent border-2 border-opacity-30"
+          on:click={() => {
+            searchPrompt = "";
+            filterCards();
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 opacity-70"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            ><path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            /></svg
+          >
+        </button>
+      </div>
+      <ThemePicker />
     </div>
+  </div>
 </div>
