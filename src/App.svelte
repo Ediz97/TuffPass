53fbe98ee81fa8ec1071e6435d9b680a8405b227
Ediz Turan <edizturan97@gmail.com>
2023-03-26 15:03:10 +0200

    message when no accounts

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
-	import AccountCards from "./lib/AccountCards.svelte";
-	import NavigationBar from "./lib/NavigationBar.svelte";
+  import { onMount } from "svelte";
+  import AccountCards from "./lib/AccountCards.svelte";
+  import NavigationBar from "./lib/NavigationBar.svelte";
+  import { userAccounts } from "./lib/stores";
+
+  onMount(async () => {
+    userAccounts.set(await window.electronAPI.getAccounts());
+  });
 </script>
 
-<main class="my-5" style="font-family: 'Caviar Dreams', sans-serif;">
-	<NavigationBar />
-	<AccountCards />
+<main style="font-family: 'Caviar Dreams', sans-serif;">
+  {#if $userAccounts.length === 0}
+    <div
+      class="flex justify-center items-center absolute w-full text-center"
+      style="height: calc(100vh - 20px)"
+    >
+      <p class="select-none opacity-50">Click the + button to add accounts!</p>
+    </div>
+  {/if}
+  <NavigationBar />
+  <AccountCards />
 </main>
