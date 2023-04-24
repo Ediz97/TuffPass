b13eee644c8b90e87c95d10fed2e21864e3c9c03
Ediz Turan <edizturan97@gmail.com>
2023-04-24 22:16:45 +0200

    other login screen for 1st time use of app

--- electron/file-io.cjs
+++ electron/file-io.cjs
@@ -1,14 +1,20 @@
-const { app } = require('electron');
-const fs = require('fs');
-const path = require('path');
+const { app } = require("electron");
+const fs = require("fs");
+const path = require("path");
 
-const userAccounts = path.join(app.getPath("appData"), "../Local/Programs/TuffPass/userAccounts.json");
+const userAccounts = path.join(
+  app.getPath("appData"),
+  "../Local/Programs/TuffPass/userAccounts.json"
+);
 
 module.exports.updateAccounts = (accounts) => {
-    fs.writeFileSync(userAccounts, JSON.stringify(accounts));
-}
+  fs.writeFileSync(userAccounts, JSON.stringify(accounts));
+};
 
-module.exports.getAccounts = (err) => {
-    if (err) throw err;
-    return JSON.parse(fs.readFileSync(userAccounts, 'utf-8'));
-}
+module.exports.getAccounts = () => {
+  try {
+    return JSON.parse(fs.readFileSync(userAccounts, "utf-8"));
+  } catch (e) {
+    return null;
+  }
+};
--- src/lib/LoginScreen.svelte
+++ src/lib/LoginScreen.svelte
@@ -6,6 +6,7 @@
   import { fly, fade } from "svelte/transition";
   import { passwordCorrect } from "./stores";
 
+  // svelte animation
   const passwordStrength = tweened(0, {
     duration: 400,
     easing: cubicOut,
@@ -13,63 +14,99 @@
 
   let applicationStarted = false;
   let masterPassword = "";
-  let masterPasswordConfirmation = "";
+  let newPassword = "";
+  let newPasswordConfirmation = "";
+  let firstRun = true;
 
-  function submitMasterPassword() {
-    if (masterPassword === masterPasswordConfirmation) {
-        passwordCorrect.set(true);
+  function createMasterPassword() {
+    if (newPassword === newPasswordConfirmation) {
+      masterPassword = newPassword;
+      passwordCorrect.set(true);
     }
   }
 
-  onMount(() => {
-    applicationStarted = true;
+  function masterPasswordCheck() {
+    // if (hashed master password is true) { TODO: implement hashed master password check
+    passwordCorrect.set(true);
+    // }
+  }
+
+  onMount(async () => {
+    applicationStarted = true; // set to true on startup, in order for the flying animation to work
+    firstRun = (await window.electronAPI.getAccounts()) === null ? true : false;
   });
 </script>
 
+<!-- Loading the component immediately on startup, in order for the animation to work -->
 {#if applicationStarted}
-<div class="hero min-h-screen absolute overflow-hidden">
-  <div class="hero-content text-center">
-    <div class="max-w-md" in:fly="{{ y: 50, duration: 700}}" out:fade="{{ duration: 300 }}">
-      <h1 class="text-5xl font-bold">Welcome!</h1>
-      <p class="py-6">
-        Please choose your master password. Make sure it's secure and remember it
-        well, since there's no way of resetting it. If you wish to use this
-        application without a master password, you can do so by leaving the
-        password fields empty.
-      </p>
+  <div class="hero min-h-screen absolute">
+    <div class="hero-content text-center">
+      <div
+        class="max-w-md"
+        in:fly={{ y: 50, duration: 700 }}
+        out:fade={{ duration: 300 }}
+      >
+        {#if firstRun}
+          <h1 class="text-5xl font-bold">Welcome!</h1>
+          <p class="py-6">
+            Please choose your master password. Make sure it's secure and
+            remember it well, since there's no way of resetting it. If you wish
+            to use this application without a master password, you can do so by
+            leaving the password fields empty.
+          </p>
 
-      <!-- Password Fields -->
-      <label class="label mt-2" for="password">
-        <span class="label-text ml-1">Password</span>
-      </label>
-      <input
-        type="text"
-        placeholder="Enter password"
-        bind:value={masterPassword}
-        on:input={() => passwordStrength.set(passwordCheck(masterPassword)[0])}
-        class="input input-bordered w-full"
-      />
+          <!-- Password Fields -->
+          <label class="label mt-2" for="password">
+            <span class="label-text ml-1">Password</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter password"
+            bind:value={newPassword}
+            on:input={() => passwordStrength.set(passwordCheck(newPassword)[0])}
+            class="input input-bordered w-full"
+          />
 
-      <label class="label mt-2" for="passwordConfirmation">
-        <span class="label-text ml-1">Confirm password</span>
-      </label>
-      <input
-        type="text"
-        placeholder="Confirm password"
-        bind:value={masterPasswordConfirmation}
-        on:input={() => passwordStrength.set(passwordCheck(masterPassword)[0])}
-        class="input input-bordered w-full"
-      />
+          <label class="label mt-2" for="passwordConfirmation">
+            <span class="label-text ml-1">Confirm password</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Confirm password"
+            bind:value={newPasswordConfirmation}
+            class="input input-bordered w-full"
+          />
 
-      <!-- Password Strength Meter -->
-      <progress
-        class="progress progress-primary w-full mt-3 px-1"
-        value={$passwordStrength}
-      />
-      <p class="text-xs">{@html passwordCheck(masterPassword)[1]}</p>
+          <!-- Password Strength Meter -->
+          <progress
+            class="progress progress-primary w-full mt-3 px-1"
+            value={$passwordStrength}
+          />
+          <p class="text-xs">{@html passwordCheck(newPassword)[1]}</p>
 
-      <button class="btn btn-success mt-10" on:click={submitMasterPassword}>Confirm</button>
+          <button class="btn btn-success mt-10" on:click={createMasterPassword}
+            >Confirm</button
+          >
+        {:else}
+          <h1 class="text-5xl font-bold">Welcome back!</h1>
+          <p class="py-6">Please enter your master password.</p>
+
+          <!-- Password Fields -->
+          <label class="label mt-2" for="password">
+            <span class="label-text ml-1">Password</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter password"
+            bind:value={masterPassword}
+            class="input input-bordered w-full"
+          />
+
+          <button class="btn btn-success mt-10" on:click={masterPasswordCheck}
+            >Confirm</button
+          >
+        {/if}
+      </div>
     </div>
   </div>
-</div>
 {/if}
